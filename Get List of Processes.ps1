296378a3ad74975d98e7475567e67240347b8ada
Bilel <forothers@sent.com>
2020-09-02 14:30:21 +0100

    Get List of Processes
    Get List of Processes

---
+++ Get List of Processes.ps1
@@ -0,0 +1,19 @@
+﻿Get-WMIObject -Class Win32_Process | Get-Member
+$taskList = @()
+foreach ($task in $taskSnapshot){
+    $taskProps = @{
+        'SID'=$task.SessionId
+        'Name'=$task.ProcessName
+        'PID'=$task.ProcessId
+        'Handles'=$task.Handles
+        'WorkingSet64'=$task.WorkingSet64
+        'Description'=$task.Description
+        'Path'=$task.Path
+        
+
+    }
+    $taskObject = New-Object -TypeName PSObject -Property $taskProps
+    $taskList += $taskObject
+}
+
+$taskList | Out-GridView
